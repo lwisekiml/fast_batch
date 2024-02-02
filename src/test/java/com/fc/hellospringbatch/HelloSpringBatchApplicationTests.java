b7829a9377e3ec25dc5a359ed56b86d0b5736237
wisekim <lwisekiml@naver.com>
2024-02-03 07:54:16 +0900

    프로젝트 세팅전

---
+++ src/test/java/com/fc/hellospringbatch/HelloSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fc.hellospringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
